7ae30809c5cdd89c31466414dea76bca56637beb
L4TFr3on <63290816+abdelrahemmekky-a0x404@users.noreply.github.com>
2022-12-29 00:24:13 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Page Title</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="index.css">
+</head>
+<body>
+  <div class="navigation">
+    <ul>
+      <li class="list active">
+        <a href="#">
+          <span class="icon">
+            <ion-icon name="home-outline"></ion-icon>
+          </span>
+          <span class="text"></span>
+        </a>
+      </li>
+      <li class="list">
+        <a href="home.html">
+          <span class="icon">
+            <ion-icon name="home-outline"></ion-icon>
+          </span>
+          <span class="text">login</span>
+        </a>
+      </li>
+      <li class="list">
+        <a href="registration.php">
+          <span class="icon">
+            <ion-icon name="log-in-outline"></ion-icon>
+          </span>
+          <span class="text">Game</span>
+        </a>
+      </li>
+      <li class="list">
+        <a href="game.html">
+          <span class="icon">
+          <ion-icon name="game-controller-outline"></ion-icon>
+
+          </span>
+        </a>
+      </li>
+      <div class="indicator"></div>
+    </ul>
+  </div>
+  
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+
+</body>
+</html>
